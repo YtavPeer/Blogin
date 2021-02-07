3452f682521f42ed5ec58fbf6a5c1ae9070549c6
YtavPeer <ytav.peer@gmail.com>
2021-02-07 19:39:10 +0200

    v1

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+function toggleMenu() {
+      var menuOpen = document.body.classList.toggle('menu-open')
+      if (menuOpen) {
+            document.querySelector('.menu-btn').innerText = 'X';
+      } else {
+            document.querySelector('.menu-btn').innerText = '☰';
+      }
+}
+
+
+function openReadModal() {
+      document.body.classList.add('modal-open')
+
+}
+
+
+function closeReadModal(ev) {
+      ev.stopPropagation()
+      // ev.preventDefault()
+      document.body.classList.remove('modal-open')
+      // document.querySelector('body').classList.remove('modal-open')
+      // document.body.classList.remove('modal-open');
+}
+
